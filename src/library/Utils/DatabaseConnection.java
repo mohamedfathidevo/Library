8e57b19d5c9f39ba4060d5aecc706761957eb175
mohamedfathidevo <mohamedfathidevo@gmail.com>
2020-05-31 23:56:23 +0200

    mohamedfathidevo

--- src/library/Utils/DatabaseConnection.java
+++ src/library/Utils/DatabaseConnection.java
@@ -6,8 +6,6 @@ package library.Utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
